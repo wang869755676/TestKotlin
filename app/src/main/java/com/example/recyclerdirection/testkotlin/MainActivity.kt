adcd9ab1e02afe9772a39e320cde788fb284019b
869755676@qq.com <869755676@qq.com>
2017-05-22 15:35:38 +0800

    简单的使用

---
+++ app/src/main/java/com/example/recyclerdirection/testkotlin/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.recyclerdirection.testkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        main_text.text=("PowerFullKotlin")
+        fun printLength(obj: Any) {
+            println("'$obj' string length is ${getStringLength(obj) ?: "... err, not a string"} ")
+        }
+        printLength("Incomprehensibilities")
+        printLength(1000)
+        printLength(listOf(Any()))
+    }
+
+    /**
+     *  定义一个函数
+     */
+    fun sum(a:Int,b:Int) : Int{
+        // 定义局部变量
+        val c :Int=1 // 立即初始化
+        val b=2  //推导出Int型
+
+        return a+b
+    }
+    fun getStringLength(obj: Any): Int? {
+        if (obj is String) {
+            // obj 将会在这个分支中自动转换为 String 类型
+            return obj.length
+        }
+
+        // obj 在种类检查外仍然是 Any 类型
+        return null
+    }
+
+
+
+}
